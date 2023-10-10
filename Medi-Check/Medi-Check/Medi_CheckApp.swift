c999c5f3ef4122f021d485f46adfe5559eefdea8
KyungsooLee <jmtkd9196@gmail.com>
2023-10-10 21:00:35 +0900

    [main]feat: 프로젝트 생성

---
+++ Medi-Check/Medi-Check/Medi_CheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  Medi_CheckApp.swift
+//  Medi-Check
+//
+//  Created by Kyungsoo Lee on 2023/10/10.
+//
+
+import SwiftUI
+
+@main
+struct Medi_CheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
