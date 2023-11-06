41db0796474427f4817180a136d87547b91310f0
KyungsooLee <jmtkd9196@gmail.com>
2023-11-07 03:27:31 +0900

    feat: 닉네임 입력 뷰 구현

---
+++ Medi-Check/Medi-Check/Presentation/AddProfileView/View/InputNicknameView.swift
@@ -0,0 +1,58 @@
+//
+//  InputNicknameView.swift
+//  Medi-Check
+//
+//  Created by Kyungsoo Lee on 11/7/23.
+//
+
+import SwiftUI
+
+struct InputNicknameView: View {
+    @State var nickname: String
+    var body: some View {
+        GeometryReader { geometry in
+            let geoWidth = geometry.size.width
+            let geoHeight = geometry.size.height
+            ZStack {
+                FirstBackgroundView()
+                VStack {
+                    Text("메디체크에서 사용할\n닉네임을 입력해주세요.")
+                        .font(.system(size: CGFloat.adaptiveSize(portraitIPhone: geoWidth * 0.08, landscapeIPhone: geoWidth * 0.04, portraitIPad: geoWidth * 0.05, landscapeIPad: geoWidth * 0.05), weight: .bold))
+                        .padding(EdgeInsets(top: geoHeight * 0.2, leading: 0, bottom: geoHeight * 0.15, trailing: 0))
+                    
+                    VStack(alignment: .leading) {
+                        Text("닉네임")
+                            .font(.system(size: CGFloat.adaptiveSize(portraitIPhone: geoWidth * 0.05, landscapeIPhone: geoWidth * 0.03, portraitIPad: geoWidth * 0.02, landscapeIPad: geoWidth * 0.02), weight: .bold))
+                            .foregroundStyle(Color.gray)
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: geoHeight * 0.05, trailing: 0))
+                        
+                        TextField("숫자, 특수문자, 이모티콘 모두 사용 가능", text: $nickname)
+                            .font(.system(size: CGFloat.adaptiveSize(portraitIPhone: geoWidth * 0.06, landscapeIPhone: geoWidth * 0.04, portraitIPad: geoWidth * 0.025, landscapeIPad: geoWidth * 0.025), weight: .bold))
+                        
+                        Rectangle()
+                            .frame(height: 1.5)
+                    }
+                    .frame(width: CGFloat.adaptiveSize(portraitIPhone: geoWidth * 0.85, landscapeIPhone: geoWidth * 0.63, portraitIPad: geoWidth * 0.5, landscapeIPad: geoWidth * 0.5), alignment: .leading)
+                    .foregroundStyle(Color.gray)
+                    
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        BasicButtonLabel(text: "완료", strokeWidth: 1, fontSize: CGFloat.adaptiveSize(portraitIPhone: geoWidth * 0.1, landscapeIPhone: geoWidth * 0.05, portraitIPad: geoWidth * 0.07, landscapeIPad: geoWidth * 0.05), width: geoWidth, height: geoHeight * 0.07)
+                            .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
+                            .background(Color.MediCheckMainColor)
+                    }
+                }
+            }
+        }
+        .onTapGesture {
+            self.endTextEditing()
+        }
+    }
+    
+}
+
+#Preview {
+    InputNicknameView(nickname: "숫자, 특수문자, 이모티콘 모두 사용 가능")
+}
--- Medi-Check/Medi-Check/Presentation/SelectProfileView/View/SelectProfileView.swift
+++ Medi-Check/Medi-Check/Presentation/SelectProfileView/View/SelectProfileView.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct SelectProfileView: View {
-//    let dubbyProfiles: [Profile] = []
+    //    let dubbyProfiles: [Profile] = []
     let dummyProfileImages: [String] = ["Profile", "Profile", "Profile"]
     let dummyName: [String] = ["Name", "Name", "Name"]
+    @State private var isAddProfileViewPresented: Bool = false
     
     var body: some View {
         GeometryReader { geometry in
@@ -18,17 +19,41 @@ struct SelectProfileView: View {
             let geoHeight = geometry.size.height
             ZStack {
                 SecondBackgroundView()
-                VStack {
+                VStack(spacing: 10) {
                     Text("프로필을 선택하세요.")
                         .font(.system(size: CGFloat.adaptiveSize(portraitIPhone: geoWidth * 0.08, landscapeIPhone: geoWidth * 0.04, portraitIPad: geoWidth * 0.07, landscapeIPad: geoWidth * 0.05), weight: .bold))
                         .padding(EdgeInsets(top: geoHeight * 0.1, leading: 0, bottom: 0, trailing: 0))
-                    HStack(spacing: 10) {
+                    HStack(spacing: CGFloat.adaptiveSize(portraitIPhone: 10, landscapeIPhone: 10, portraitIPad: 20, landscapeIPad: 20)) {
                         ForEach(dummyProfileImages.indices, id: \.self) { index in
                             ProfileView(image: dummyProfileImages[index], name: dummyName[index])
                                 .frame(width: CGFloat.adaptiveSize(portraitIPhone: geoWidth * 0.3, landscapeIPhone: geoWidth * 0.3, portraitIPad: geoWidth * 0.3, landscapeIPad: geoWidth * 0.3), height: CGFloat.adaptiveSize(portraitIPhone: geoHeight * 0.2, landscapeIPhone: geoHeight * 0.5, portraitIPad: geoHeight * 0.25, landscapeIPad: geoHeight * 0.4))
                             
                         }
                     }
+                    
+                    VStack {
+                        Button {
+                            isAddProfileViewPresented = true
+                        } label: {
+                            ZStack {
+                                Circle()
+                                    .stroke(lineWidth: 1)
+                                    .frame(width: CGFloat.adaptiveSize(portraitIPhone: geoWidth * 0.1, landscapeIPhone: geoWidth * 0.08, portraitIPad: geoWidth * 0.1, landscapeIPad: geoWidth * 0.1))
+                                Image(systemName: "plus")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: CGFloat.adaptiveSize(portraitIPhone: geoWidth * 0.05, landscapeIPhone: geoWidth * 0.05, portraitIPad: geoWidth * 0.05, landscapeIPad: geoWidth * 0.05))
+                            }
+                            .foregroundStyle(Color.black)
+                            .navigationDestination(isPresented: $isAddProfileViewPresented) {
+                                
+                            }
+                            
+                        }
+                        Text("프로필 추가")
+                            .font(.system(size: CGFloat.adaptiveSize(portraitIPhone: 15, landscapeIPhone: 15, portraitIPad: 40, landscapeIPad: 40), weight: .bold))
+                    }
+                    
                 }
             }
         }
