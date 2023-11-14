5728d9eff7edda22ef9bd53ed435097007589c4a
KyungsooLee <jmtkd9196@gmail.com>
2023-11-15 02:32:48 +0900

    feat: 가족 코드가 동일한 멤버 불러오기 API 구현

---
+++ Medi-Check/Medi-Check/Presentation/SelectProfileView/ViewModel/SelectProfileViewModel.swift
@@ -0,0 +1,79 @@
+//
+//  SelectProfileViewModel.swift
+//  Medi-Check
+//
+//  Created by Kyungsoo Lee on 11/15/23.
+//
+
+import Foundation
+
+fileprivate enum MediCheckAPI {
+    static let scheme = "http"
+    static let host = "yuno.hopto.org"
+    
+    enum Path: String {
+        case members = "/members"
+    }
+    
+}
+
+class SelectProfileViewModel: ObservableObject {
+    @Published var members: [getMembersDTO] = []
+    
+    @MainActor
+    func fetchData(familyCode: String) async {
+        do {
+            self.members = try await getMembers(familyCode: familyCode)
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+    
+    func getMembers(familyCode: String) async throws -> [getMembersDTO] {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = MediCheckAPI.scheme
+        urlComponents.host = MediCheckAPI.host
+        urlComponents.port = 80
+        urlComponents.path = MediCheckAPI.Path.members.rawValue
+        urlComponents.queryItems = [URLQueryItem(name: "familyCode", value: familyCode)]
+        
+        guard let url = urlComponents.url else {
+            print("[getMembers] Error: cannot create URL")
+            throw ExchangeRateError.cannotCreateURL
+        }
+        print(url)
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+        
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        if let response = response as? HTTPURLResponse,
+           !(200..<300).contains(response.statusCode) {
+            throw ExchangeRateError.badResponse
+        }
+        
+        print(response)
+        print(data)
+//        guard let jsonDictionary = try? JSONSerialization.jsonObject(with: Data(data), options: []) as? [String: Any] else {
+//            print("[getMembers] Error: convert failed json to dictionary")
+//            throw ExchangeRateError.decodeFailed
+//        }
+//        print(jsonDictionary)
+        
+        let decoder = JSONDecoder()
+        var members: [getMembersDTO] = []
+        members = try decoder.decode([getMembersDTO].self, from: data)
+        
+        return members
+    }
+    
+    
+    
+}
+
+extension SelectProfileViewModel {
+    struct getMembersDTO: Codable {
+        let nickName: String
+        let imgUrl: String
+    }
+}
