b395c5b92601ddc4ceb5ec045f0870ab196a4e66
KyungsooLee <jmtkd9196@gmail.com>
2023-11-17 21:47:00 +0900

    fix: 대시보드 URL 수정

--- Medi-Check/Medi-Check/Presentation/HomeView/View/HomeView.swift
+++ Medi-Check/Medi-Check/Presentation/HomeView/View/HomeView.swift
@@ -16,7 +16,7 @@ struct HomeView: View {
             VStack {
                 TabView(selection: $indexOfView) {
                     
-                    DashboardView(urlToLoad: "https://dashboard-trainingapps-eks004.sa.wise-paas.com/d/Tns3Y0ZMz/core2_1215_sk?orgId=1&refresh=3s")
+                    DashboardView(urlToLoad: "https://dashboard-trainingapps-eks004.sa.wise-paas.com/d/MUrqFy4Sk/medicheck?tab=visualization&edit&from=1700049894496&to=1700222694496&orgId=2283")
                         .ignoresSafeArea()
                         .tabItem {
                             Image(systemName: "chart.bar.xaxis")
