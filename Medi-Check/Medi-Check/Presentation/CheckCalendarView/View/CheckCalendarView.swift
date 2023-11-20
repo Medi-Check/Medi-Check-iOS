80365e688126e889ae8379cca0787eb6581bb65e
KyungsooLee <jmtkd9196@gmail.com>
2023-11-21 01:57:21 +0900

    feat: 요일별 일정 조회 구현

--- Medi-Check/Medi-Check/Presentation/CheckCalendarView/View/CheckCalendarView.swift
+++ Medi-Check/Medi-Check/Presentation/CheckCalendarView/View/CheckCalendarView.swift
@@ -11,12 +11,30 @@ struct CheckCalendarView: View {
     @ObservedObject var viewModel = CheckCalendarViewModel()
     @EnvironmentObject var userData: UserData
     @State private var isSheetPresented: Bool = false
+    @State private var selectWeekDay: String = "월"
+    @State private var weekDictionary: [String: String] = ["월": "MONDAY", "화": "TUESDAY", "수": "WEDNESDAY", "목": "THURSDAY", "금": "FRIDAY", "토": "SATURDAY", "일": "SUNDAY", "매일": "EVERYDAY"]
+    @State private var filteringWeekArray: [CheckCalendarViewModel.getScheduleDTO] = []
+    
+    let weekdays: [String] = ["월", "화", "수", "목", "금", "토", "일", "매일"]
     
     var body: some View {
         VStack {
+            HStack {
+                ForEach(weekdays.indices, id: \.self) { index in
+                    let buttonColor = weekdays[index] == selectWeekDay ? Color.gray : Color.white
+                    Button {
+                        selectWeekDay = weekdays[index]
+                        filteringWeekArray = viewModel.schedules.filter { $0.week == weekDictionary[selectWeekDay] }
+                        print("필터링 테스트 : \(filteringWeekArray)")
+                    } label: {
+                        Text(weekdays[index])
+                            .background(buttonColor)
+                    }
+                }
+            }
             ScrollView(.vertical) {
-                ForEach(viewModel.schedules.indices, id: \.self) { index in
-                    let schedule = viewModel.schedules[index]
+                ForEach(filteringWeekArray.indices, id: \.self) { index in
+                    let schedule = filteringWeekArray[index]
                     HStack {
                         Button {
                             
@@ -55,6 +73,7 @@ struct CheckCalendarView: View {
         .onAppear {
             Task {
                 await viewModel.fetchData(memberName: userData.currnetProfile.nickName)
+                filteringWeekArray = viewModel.schedules.filter { $0.week == weekDictionary["월"] }
             }
         }
     }
